00314874cff778a351fae03471124e42e46f4028
satoshi_kumano <thebearsfield@gmail.com>
2020-07-14 23:22:29 +0900

    Docker起動時にDBにデータ挿入する処理を追加

--- mysql/initdb.d/myblog.sql
+++ mysql/initdb.d/myblog.sql
@@ -103,3 +103,73 @@ DELIMITER ;
 
 # userデータ挿入
 INSERT INTO user(name, email, password) VALUES('test-user', 'test@test.com', '$2y$10$KSxv6HgNfjTdbP7P/KbGXeKQ4FzXvlPhSDekqJhpsdU0DAIpaR64G');
+
+# postデータ挿入
+INSERT INTO posts(title, post) VALUES('test1', 'test1');
+INSERT INTO posts(title, post) VALUES('test2', 'test2');
+INSERT INTO posts(title, post) VALUES('test3', 'test3');
+INSERT INTO posts(title, post) VALUES('test4', 'test4');
+INSERT INTO posts(title, post) VALUES('test5', 'test5');
+INSERT INTO posts(title, post) VALUES('test6', 'test6');
+INSERT INTO posts(title, post) VALUES('test7', 'test7');
+INSERT INTO posts(title, post) VALUES('test8', 'test8');
+INSERT INTO posts(title, post) VALUES('test9', 'test9');
+INSERT INTO posts(title, post) VALUES('test10', 'test10');
+INSERT INTO posts(title, post) VALUES('test11', 'test11');
+
+# postとuserの紐付け
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 1);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 2);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 3);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 4);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 5);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 6);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 7);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 8);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 9);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 10);
+INSERT INTO user_uploaded_posts(user_id, post_id) VALUES(1, 11);
+
+# tagデータ挿入、およびtagとpostの紐付け
+CALL sp_add_tags('PHP', 1);
+CALL sp_add_tags('MYSQL', 1);
+CALL sp_add_tags('Linux', 1);
+CALL sp_add_tags('nginx', 1);
+CALL sp_add_tags('PHP', 2);
+CALL sp_add_tags('Laravel', 2);
+CALL sp_add_tags('MYSQL', 2);
+CALL sp_add_tags('Linux', 2);
+CALL sp_add_tags('Apache', 2);
+CALL sp_add_tags('Ruby', 3);
+CALL sp_add_tags('MYSQL', 3);
+CALL sp_add_tags('RubyOnRails', 3);
+CALL sp_add_tags('Javascript', 4);
+CALL sp_add_tags('Vue', 4);
+CALL sp_add_tags('React', 4);
+CALL sp_add_tags('jQuery', 4);
+CALL sp_add_tags('Docker', 5);
+CALL sp_add_tags('DockerCompose', 5);
+CALL sp_add_tags('Kubernetes', 5);
+CALL sp_add_tags('PHP', 6);
+CALL sp_add_tags('MYSQL', 6);
+CALL sp_add_tags('Linux', 6);
+CALL sp_add_tags('nginx', 6);
+CALL sp_add_tags('PHP', 7);
+CALL sp_add_tags('Laravel', 7);
+CALL sp_add_tags('MYSQL', 7);
+CALL sp_add_tags('Linux', 7);
+CALL sp_add_tags('Apache', 7);
+CALL sp_add_tags('Ruby', 8);
+CALL sp_add_tags('MYSQL', 8);
+CALL sp_add_tags('RubyOnRails', 8);
+CALL sp_add_tags('Javascript', 9);
+CALL sp_add_tags('Vue', 9);
+CALL sp_add_tags('React', 9);
+CALL sp_add_tags('jQuery', 9);
+CALL sp_add_tags('Docker', 10);
+CALL sp_add_tags('DockerCompose', 10);
+CALL sp_add_tags('Kubernetes', 10);
+CALL sp_add_tags('PHP', 11);
+CALL sp_add_tags('MYSQL', 11);
+CALL sp_add_tags('Linux', 11);
+CALL sp_add_tags('nginx', 11);
