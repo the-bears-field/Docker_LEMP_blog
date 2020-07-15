75a1b2b38c1b096a402aeea0904a8b1a845eb77d
satoshi_kumano <thebearsfield@gmail.com>
2020-07-16 00:45:58 +0900

    iPhoneで編集後に投稿した際に、不正アクセスとなる不具合を修正

--- www/html/edit.php
+++ www/html/edit.php
@@ -20,6 +20,11 @@ function trimmingWords($string)
     return $string;
 }
 
+if (!isset($_POST['posting'])) {
+    $token = sha1(uniqid(random_bytes(16), true));
+    $_SESSION['token'] = $token;
+}
+
 if (isset($_GET['postID'])) {
     $postID = intval($_GET['postID']);
     $sqlCommand  = "SELECT posts.post_id, posts.title, posts.post, GROUP_CONCAT(tags.tag_name SEPARATOR ' ') AS tags, user_uploaded_posts.user_id AS user_id FROM posts
@@ -42,11 +47,6 @@ if (isset($_GET['postID'])) {
 if (!isset($_GET['postID']) || $stmt['user_id'] !== $_SESSION['id']) {
     header('location: /');
     exit();
-}
-
-if (!isset($_POST['posting'])) {
-    $token = sha1(uniqid(random_bytes(16), true));
-    $_SESSION['token'] = $token;
 }
 
 if (isset($_POST['posting'])) {
