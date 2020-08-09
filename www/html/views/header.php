08eaa4f14fa66f1d93ef095ca205c270d867219d
satoshi_kumano <thebearsfield@gmail.com>
2020-08-10 01:22:48 +0900

    trumbowygに関わるファイルのパスを変更

--- www/html/views/header.php
+++ www/html/views/header.php
@@ -5,9 +5,9 @@
 <link rel="stylesheet" href="/assets/stylesheets/styles.css">
 <link rel="stylesheet" href="/assets/stylesheets/styles-tb.css">
 <link rel="stylesheet" href="/assets/stylesheets/styles-sp.css">
-<link rel="stylesheet" href="./assets/javascripts/trumbowyg/dist/ui/trumbowyg.min.css">
+<link rel="stylesheet" href="./node_modules/trumbowyg/dist/ui/trumbowyg.min.css">
 <link rel="stylesheet" href="./assets/stylesheets/fontawesome.css">
 <script src="./assets/javascripts/jquery.min.js"></script>
-<script src="./assets/javascripts/trumbowyg/dist/trumbowyg.min.js"></script>
-<script src="./assets/javascripts/trumbowyg/dist/langs/ja.min.js"></script>
+<script src="./node_modules/trumbowyg/dist/trumbowyg.min.js"></script>
+<script src="./node_modules/trumbowyg/dist/langs/ja.min.js"></script>
 <script src="./assets/javascripts/myblog.js"></script>
